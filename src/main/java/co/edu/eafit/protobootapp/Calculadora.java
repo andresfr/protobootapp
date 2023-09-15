05a205f3f38577f369260881435323e7b2ac39de
Andres Ramirez <andresfr@gmail.com>
2023-09-15 18:10:37 -0500

    actualizacion2

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -2,6 +2,7 @@ package co.edu.eafit.protobootapp;
 
 //This is a simple calculator class.
 //Actualización Andres
+//actualizacion 2
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
